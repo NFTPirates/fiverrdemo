a1735198cfadca24aa4f9388d6b77a8ae7de2361
Mateiasul <potcovarumatei@gmail.com>
2024-04-04 11:49:18 +0100

    update input box to handle crazy small number

--- app/components/ConversionBox/ConversionBox.tsx
+++ app/components/ConversionBox/ConversionBox.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Coin } from '@/app/types/coin';
 import { Currency } from '@/app/types/currency';
+import { formatNumberToString } from '@/app/utils/utils';
 import { Input } from '@nextui-org/react';
 import { Dispatch, SetStateAction } from 'react';
 import NextUiDropdown from '../Dropdown/Dropdown';
@@ -22,7 +23,9 @@ export default function ConversionBox(props: IConversionBoxProps) {
             ></NextUiDropdown>
             <Input
                 className={styles.container__input}
-                value={props.coinAmount?.toString()}
+                value={formatNumberToString({
+                    numberToFormat: props.coinAmount,
+                })}
                 size={'lg'}
                 type="number"
                 label="Amount"
